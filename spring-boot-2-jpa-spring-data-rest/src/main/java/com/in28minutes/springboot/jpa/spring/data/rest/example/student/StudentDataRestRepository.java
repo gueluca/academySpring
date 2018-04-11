0a68a8c85fdb97aa7ea95e06672a6efd5a98ccc7
peps980 <giuseppe.alaimo1985@gmail.com>
2018-04-11 19:02:14 +0200

    CRUD fix

--- spring-boot-2-jpa-spring-data-rest/src/main/java/com/in28minutes/springboot/jpa/spring/data/rest/example/student/StudentDataRestRepository.java
+++ spring-boot-2-jpa-spring-data-rest/src/main/java/com/in28minutes/springboot/jpa/spring/data/rest/example/student/StudentDataRestRepository.java
@@ -3,7 +3,7 @@ package com.in28minutes.springboot.jpa.spring.data.rest.example.student;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "profiles", collectionResourceRel = "profiles")
+@RepositoryRestResource(path = "modifica", collectionResourceRel = "modifica")
 public interface StudentDataRestRepository extends PagingAndSortingRepository<Student, Long>{
 
 }
